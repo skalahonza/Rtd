a45c2bf32742e12d3fbdde536ee1754a4e530b70
Vit Neckar <neckavit@fel.cvut.cz>
2017-10-23 22:15:30 +0200

    basic menu

---
+++ Rtd/Assets/Scripts/MouseHover.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseHover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+	
+	// Update is called once per frame
+	void OnMouseEnter () {
+        GetComponent<Renderer>().material.color = Color.red;
+    }
+
+    private void OnMouseExit() {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
