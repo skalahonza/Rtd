7f51d919e34449fcb63b9bf823c070a26f4a0aa5
Michal Filo <mido@alavio.eu>
2017-10-23 23:56:20 +0200

    added LobbyHUD

--- Rtd/Assets/Multiplayer/Lobby.cs
+++ Rtd/Assets/Multiplayer/Lobby.cs
@@ -4,13 +4,4 @@ using UnityEngine;
 using UnityEngine.Networking;
 public class Lobby : NetworkLobbyManager {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
 }
