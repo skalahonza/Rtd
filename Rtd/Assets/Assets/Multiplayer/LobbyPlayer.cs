81a93808c51b67a927e75eead70216eed22cffd6
Michal Filo <mido@alavio.eu>
2017-10-23 22:08:27 +0200

    added LobbyPlayer

--- Rtd/Assets/Assets/Multiplayer/LobbyPlayer.cs
+++ Rtd/Assets/Assets/Multiplayer/LobbyPlayer.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-public class Lobby : NetworkLobbyManager {
+public class LobbyPlayer : NetworkLobbyPlayer {
 
 	// Use this for initialization
 	void Start () {
