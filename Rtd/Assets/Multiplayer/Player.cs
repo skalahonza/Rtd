202df8e3538137e345b68ae110a717cfb539208c
Michal Filo <mido@alavio.eu>
2017-10-25 22:16:13 +0200

    multiplayer compliance on cars

--- Rtd/Assets/Multiplayer/Player.cs
+++ Rtd/Assets/Multiplayer/Player.cs
@@ -2,7 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Networking;
-public class Player  {
+using UnityStandardAssets.Utility;
 
-
+public class Player : NetworkBehaviour {
+    public override void OnStartLocalPlayer()
+     {
+        //GameObject.Find("Camera").GetComponent(SmoothFollow).target = transform;
+     }
 }
+     
