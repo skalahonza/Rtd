c45e43a5b21a481a42ea0a4028a200a44c6be94a
Jan Skála <j.skala@hotmail.com>
2017-12-20 14:28:54 +0100

    Fix missile and reverse missile powerup proejctile.

--- Rtd/Assets/Scripts/Powerups/Projectiles/MissilePowerupProjectile.cs
+++ Rtd/Assets/Scripts/Powerups/Projectiles/MissilePowerupProjectile.cs
@@ -8,5 +8,17 @@ namespace Assets.Scripts.Powerups.Projectiles
         {
             return Resources.Load<GameObject>("missile_powerup");            
         }
+
+        public override void OnHit(Collider other)
+        {
+            var parent = other.transform.gameObject;
+            if (parent.Equals(Owner)) return;
+            IDamagable target;
+            if ((target = parent.GetComponent<IDamagable>()) != null)
+            {
+                target.SufferDamage(this);
+            }
+            base.OnHit(other);
+        }
     }
 }
---
+++ Rtd/Assets/Scripts/Powerups/Projectiles/SurgePowerupProjectile.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts.Powerups.Projectiles
+{
+    public class SurgePowerupProjectile : ProjectileBase {
+        public override GameObject GetPrefab()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
