b01e02be68cda008ea6222062027e05f4db5cf2d
Vit Neckar <neckavit@fel.cvut.cz>
2018-01-01 20:47:46 +0100

    ai test sped

--- Rtd/Assets/Scripts/Game/AIPlayer.cs
+++ Rtd/Assets/Scripts/Game/AIPlayer.cs
@@ -17,7 +17,7 @@ class AIPlayer : Player{
         //agent.updateRotation = false;
         agent.autoBraking = false;
         //agent.acceleration = spirit.MaxMotorTorque;
-        agent.speed = spirit.maxSpeed/2;
+        agent.speed = /*spirit.maxSpeed/2*/80;
         agent.angularSpeed = 30;
         agent.height = 5.5f;
         agent.radius = 8.0f;
