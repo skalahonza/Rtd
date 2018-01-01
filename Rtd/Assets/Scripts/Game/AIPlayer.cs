09020eede67b5f7190a1521a01ac698ba590b924
Michal Filo <mido@alavio.eu>
2018-01-01 19:53:41 +0100

    AI fix HUD fix

--- Rtd/Assets/Scripts/Game/AIPlayer.cs
+++ Rtd/Assets/Scripts/Game/AIPlayer.cs
@@ -27,8 +27,8 @@ class AIPlayer : Player{
 
     void FixedUpdate() {
         if(!startRace)
-            re
-        if(map.checkpoints.Length; != checkpointOffest+1){
+            return;
+        if(map.checkpoints.Length != checkpointOffest+1){
             agent.destination = map.checkpoints[checkpointOffest+1].transform.position;
         }
         float angle = Vector3.Angle(agent.velocity, this.transform.forward);
