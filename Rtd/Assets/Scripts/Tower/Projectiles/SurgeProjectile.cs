1949d42678ec864185c15ecd901a5dc670793757
Jan Skála <j.skala@hotmail.com>
2018-01-01 14:46:55 +0100

    Surge projectile effect.

--- Rtd/Assets/Scripts/Tower/Projectiles/SurgeProjectile.cs
+++ Rtd/Assets/Scripts/Tower/Projectiles/SurgeProjectile.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using Assets.Mechanics;
+using UnityEngine;
 
 namespace Assets.Scripts.Tower.Projectiles
 {
@@ -12,6 +13,8 @@ namespace Assets.Scripts.Tower.Projectiles
 
         public override void OnHit(Collider other)
         {
+            var particle = AnimationMechanics.SpawnParticle("shockwave_projectile_effect");
+            particle.transform.position = gameObject.transform.position;
             var colliders = Physics.OverlapSphere(gameObject.transform.position, BlastRadius);
             foreach (var collider in colliders)
             {
