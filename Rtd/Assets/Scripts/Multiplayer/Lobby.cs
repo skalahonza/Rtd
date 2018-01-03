9174effc5588180fe7ecdce7afba62fc5ba1c5f5
Michal Filo <mido@alavio.eu>
2018-01-03 09:29:02 +0100

    ted uz nevim

--- Rtd/Assets/Scripts/Multiplayer/Lobby.cs
+++ Rtd/Assets/Scripts/Multiplayer/Lobby.cs
@@ -62,7 +62,7 @@ public class Lobby : NetworkLobbyManager {
 
     public override bool OnLobbyServerSceneLoadedForPlayer(GameObject lobbyPlayer, GameObject gamePlayer){
         loaded++;
-        return true;
+        return false;
     }
 
     public override void OnServerConnect(NetworkConnection conn){
