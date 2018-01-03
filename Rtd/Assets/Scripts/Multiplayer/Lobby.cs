406be6505fdfe69f09ee5c00c7ebd3b6a020e5dd
Michal Filo <mido@alavio.eu>
2018-01-03 01:05:37 +0100

    quick fix no 3

--- Rtd/Assets/Scripts/Multiplayer/Lobby.cs
+++ Rtd/Assets/Scripts/Multiplayer/Lobby.cs
@@ -93,11 +93,11 @@ public class Lobby : NetworkLobbyManager {
         go.transform.GetChild(2).GetComponent<Renderer>().material = material;
         go.transform.GetChild(3).GetComponent<Renderer>().material = material;
         go.transform.GetChild(4).GetComponent<Renderer>().material = material;
-        if(pcid == playerControllerId){
+        /*if(pcid == playerControllerId){
             SceneManager.LoadScene("HUD", LoadSceneMode.Additive);
             LocalPlayer cc = go.AddComponent<LocalPlayer>();
             cc.startRace = true;        
-        }
+        }*/
         return go;
     }
 
