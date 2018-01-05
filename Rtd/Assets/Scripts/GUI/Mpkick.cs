a889c1e17c5708c8a80e3ed346f8c251e9bc95a3
Michal Filo <mido@alavio.eu>
2018-01-05 10:28:41 +0100

    mp kick

---
+++ Rtd/Assets/Scripts/GUI/Mpkick.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Mpkick : MonoBehaviour {
+    public void UpdateDD(Button dd){
+        GameObject.Find("network").GetComponent<LobbyController>().Kick(dd);
+    }
+}
