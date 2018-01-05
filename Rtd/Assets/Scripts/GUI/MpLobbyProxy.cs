048a19d88e67e96329db7a6557487601cff8cbf9
Michal Filo <mido@alavio.eu>
2018-01-05 12:27:49 +0100

    unity bug workaround

---
+++ Rtd/Assets/Scripts/GUI/MpLobbyProxy.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MpLobbyProxy : MonoBehaviour {
+
+	public LobbyController lc;
+
+	public void Host(){
+		lc.Host();
+	}
+
+	public void Connect(){
+		lc.Connect();
+	}
+}
---
+++ Rtd/Assets/Scripts/Multiplayer/NetwokResolution.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//passing around bug 712042
+public class NetwokResolution : MonoBehaviour {
+
+	public GameObject spawn;
+	public GameObject mmaker;
+	// Use this for initialization
+	void Start () {
+		if(GameObject.Find("network") == null){
+			GameObject inst = Instantiate(Resources.Load<GameObject>("Multiplayer/network"));
+			LobbyController lc = GetComponent<MpLobbyProxy>().lc = inst.GetComponent<LobbyController>();
+			lc.lobby = inst.GetComponent<Lobby>();
+			lc.mmaker = mmaker;
+			lc.spawn = spawn;
+			inst.SetActive(true);
+			lc.mmaker.SetActive(false);
+		}
+	}
+}
