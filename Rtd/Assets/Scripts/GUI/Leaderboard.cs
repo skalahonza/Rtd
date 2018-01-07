5f98b2ab8bd2afb502ef9153251816484b24cacf
Michal Filo <mido@alavio.eu>
2018-01-07 17:10:36 +0100

    leaderboards

---
+++ Rtd/Assets/Scripts/GUI/Leaderboard.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Leaderboard : MonoBehaviour {
+    public GameObject prefab;
+    public Color[] colors = new Color[5];
+
+	private void Update() {
+        Leaderboards ld = GameObject.Find("lead").GetComponent<Leaderboards>();
+        if(ld.players.Count != transform.childCount){
+            Render(ld);
+        }
+    }
+
+    private void Render(Leaderboards ld){
+        for(int i = 0; i < transform.childCount; i++)
+        {
+            Destroy(transform.GetChild(i).gameObject);
+        }
+        int d =1;
+        foreach (var item in ld.players)
+        {
+            Text x = Instantiate(prefab, transform).GetComponent<Text>();
+            x.text = d +": <color=#" + ColorUtility.ToHtmlStringRGBA (colors[item.cid]) + ">" + item.cname + "</color>";
+            d++;
+        }
+    }
+}
