8d8a4ca3730389751071abb741f420024f8200b1
Michal Filo <mido@alavio.eu>
2018-01-07 21:51:12 +0100

    leaderboards mp fix

--- Rtd/Assets/Scripts/GUI/Leaderboard.cs
+++ Rtd/Assets/Scripts/GUI/Leaderboard.cs
@@ -6,24 +6,24 @@ using UnityEngine.UI;
 public class Leaderboard : MonoBehaviour {
     public GameObject prefab;
     public Color[] colors = new Color[5];
+    public GameObject go;
 
 	private void Update() {
         Leaderboards ld = GameObject.Find("lead").GetComponent<Leaderboards>();
-        Debug.Log(string.Format("lol {0} {1}",ld.players.Count,transform.childCount ));
-        if(ld.players.Count != transform.childCount){
+        if(ld.players.Count != go.transform.childCount){
             Render(ld);
         }
     }
 
     private void Render(Leaderboards ld){
-        for(int i = 0; i < transform.childCount; i++)
+        for(int i = 0; i < go.transform.childCount; i++)
         {
-            Destroy(transform.GetChild(i).gameObject);
+            Destroy(go.transform.GetChild(i).gameObject);
         }
         int d =1;
         foreach (var item in ld.players)
         {
-            Text x = Instantiate(prefab, transform).GetComponent<Text>();
+            Text x = Instantiate(prefab, go.transform).GetComponent<Text>();
             x.text = d +": <color=#" + ColorUtility.ToHtmlStringRGBA (colors[item.cid]) + ">" + item.cname + "</color>";
             d++;
         }
