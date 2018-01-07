dc7e0e5ecbf1dc510ea64b0b4eb4dade87033046
Jan Skála <j.skala@hotmail.com>
2018-01-07 21:57:38 +0100

    Heal animation.

--- Rtd/Assets/Scripts/Game/Checkpoint.cs
+++ Rtd/Assets/Scripts/Game/Checkpoint.cs
@@ -1,3 +1,4 @@
+using Assets.Mechanics;
 using UnityEngine;
 
 public class Checkpoint : MonoBehaviour {
@@ -13,5 +14,6 @@ public class Checkpoint : MonoBehaviour {
         //heal the car
          var car = other.gameObject.GetComponent<CarSpirit>();
          car.Hp = car.MaxHp;
+         AnimationMechanics.SpawnParticle("heal_wave", car.gameObject.transform);
      }
 }
