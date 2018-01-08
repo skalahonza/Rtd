f7dbf60211ee97d021565ae3f87386d32a193fde
Michal Filo <mido@alavio.eu>
2018-01-08 01:36:39 +0100

    ld dbg

--- Rtd/Assets/Scripts/GUI/HUD.cs
+++ Rtd/Assets/Scripts/GUI/HUD.cs
@@ -124,12 +124,12 @@ public class HUD : MonoBehaviour {
     public void RenderLeaderboards(){
         gameObject.SetActive(false); //disable all children instead and activate return button
         leaderb.transform.GetChild(0).gameObject.SetActive(true);
-
         if(Assets.Mechanics.MultiplayerHelper.IsMultiplayer()){
             leaderb.transform.GetChild(1).gameObject.SetActive(true);
         }else{
             leaderb.transform.GetChild(2).gameObject.SetActive(true);
         }
+        leaderb.GetComponent<Image>().enabled = true;
     }
 
     public void ReturnBtt(){
