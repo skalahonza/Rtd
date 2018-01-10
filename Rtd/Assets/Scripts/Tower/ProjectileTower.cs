baadd854f19acb8db97e749a5c7f61e80bed6624
Jan Skála <j.skala@hotmail.com>
2018-01-10 01:56:37 +0100

    Commend powerups.

--- Rtd/Assets/Scripts/Tower/ProjectileTower.cs
+++ Rtd/Assets/Scripts/Tower/ProjectileTower.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using Assets.Mechanics;
 using Assets.Scripts.Tower.Projectiles;
 
+/// <summary>
+/// Tower with shooting ability
+/// </summary>
 public class ProjectileTower : TowerBase
 {
     public Transform MuzzlePosition;
